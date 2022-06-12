3cb3ef674d08b6719b2ace7d239c8efd90fbc78f
Shivang <83906239+shivang1809@users.noreply.github.com>
2022-06-12 11:56:00 +0530

    Add files via upload

--- others/news.php
+++ others/news.php
@@ -1,1 +1,10 @@
+<?php
+include('simple_html_dom.php');
+$html = file_get_html('https://www.ndtv.com/search?searchtext=ev');
 
+$root =  $html->find('div#news_list',0);
+$result =  $root->find('ul.src_lst-ul',0);
+
+foreach($result->find('a') as $element)
+    echo '<li>' . $element . '</li>';
+?>
